1ed67b52921526a320ae414d776169b38c97fff3
Lim Ding Han <dinghan@Lims-MacBook-Air-2.local>
2023-03-18 19:13:49 +0800

    added box shadows

--- components/ColourfulBox.tsx
+++ components/ColourfulBox.tsx
@@ -7,7 +7,7 @@ interface Props {
 const ColourfulBox = ({children} : Props) => {
     return (
         <>
-            <div className="p-5 border-8 border-l-fuchsia-400 border-t-fuchsia-300 border-b-sky-200 border-r-sky-300">
+            <div className="p-5 border-8 border-l-fuchsia-400 border-t-fuchsia-300 border-b-sky-200 border-r-sky-300 shadow-lg shadow-rose-200">
                     {children}
             </div>
         </>
