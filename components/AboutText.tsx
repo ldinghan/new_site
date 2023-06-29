951e510df8343f62ae9b6d7c615731b1e8ebd431
ldinghan <u.dinghan@gmail.com>
2023-06-29 09:51:49 +0800

    fix apos

--- components/AboutText.tsx
+++ components/AboutText.tsx
@@ -7,7 +7,7 @@ const AboutText = () => {
     return (
         <>
             <div className="my-24 px-[1vw] min-w-screen justify-center min-h-screen items-center flex flex-col text-white font-light font-ChakraPetch">
-                <div className="mx-10 animate-blurShow"><ColourfulBox><p className="leading-relaxed text-center text-4xl sm:text-5xl font-bold">I'm Ding Han, a Year 2 NUS Computer Science undergraduate!</p></ColourfulBox></div>
+                <div className="mx-10 animate-blurShow"><ColourfulBox><p className="leading-relaxed text-center text-4xl sm:text-5xl font-bold">I&#39;m Ding Han, a Year 2 NUS Computer Science undergraduate!</p></ColourfulBox></div>
                 <div className="mt-8 grid grid-rows-2 grid-flow-col sm:gap-4 gap-2">
                     <div className="animate-blurShowLate"><ColourfulBox>
                         <ul className="leading-relaxed text-center sm:text-2xl text-xl">
